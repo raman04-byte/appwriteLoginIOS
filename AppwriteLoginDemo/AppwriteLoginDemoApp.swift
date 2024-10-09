cff57b6a9aea97ae36fc9cd19d45214646d1f056
Raman Tank <ramantank04022002@gmail.com>
2024-10-09 12:29:20 +0530

    Initial Commit

---
+++ AppwriteLoginDemo/AppwriteLoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppwriteLoginDemoApp.swift
+//  AppwriteLoginDemo
+//
+//  Created by Raman Tank on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppwriteLoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
